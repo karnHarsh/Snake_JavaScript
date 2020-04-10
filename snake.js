900f5cc0c79d4043bc7286e3769c05d61add42d2
Harsh Karn <harshkarn29@gmail.com>
2020-04-10 11:34:31 +0530

    feature/ added images of ground and food

--- snake.js
+++ snake.js
@@ -1,5 +1,16 @@
-const cvs = document.getElementById("snake");
-const ctx = cvs.getContext("2d");
+const cvs = document.getElementById("snake"); //retrieving canvas from index.html recognised by id "snake"
+const ctx = cvs.getContext("2d"); //ctx is context
 
 //create the  unit
-const box = 32;
+//unit means one square unit of canvas
+
+const box = 32; 
+
+
+//load images
+
+const ground = new Image(); //ground is an object of Image() class
+ground.src = "img/ground.png";
+
+const foodImage = new Image(); //foodImage is an object of Image() class
+foodImage.src = "img/food.png";
